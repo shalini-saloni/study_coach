908a8394a80b849a624fe53b8bdcf2f592de9363
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-02-21 15:46:55 +0530

    data encoded and scaled feature

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -17,3 +17,25 @@ def create_risk_label(df):
 
     df["risk_level"] = df["G3"].apply(risk_level)
     return df
+
+def clean_and_encode(df, target="G3", task="regression"):
+    df = df.drop_duplicates()
+
+    if target == "G3":
+        if "G1" in df.columns:
+            df = df.drop(columns=["G1", "G2"])
+
+    y = df[target]
+    X = df.drop(columns=[target])
+
+    X = pd.get_dummies(X, drop_first=True)
+
+    return X, y
+
+def scale_features(X_train, X_test):
+    scaler = StandardScaler()
+
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    return X_train_scaled, X_test_scaled, scaler
